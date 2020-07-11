86c45e3f38a9cd13a7866a5bd5ca5359c8de309c
gsaiprathyusha <saiprathyushagidugu71847@gmail.com>
2020-07-11 20:20:49 +0530

    updated api

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,jsonify
+from flask_restful import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return {"message" : "Deployed!"}
+
+api.add_resource(Home, "/")
+
+app.run(debug=True)
